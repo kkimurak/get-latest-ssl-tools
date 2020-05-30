0c56c34e8d0b7f91b8589b57ec73a678186c90c5
Kazunori Kimura <norytama.k2@gmail.com>
2020-05-30 22:55:36 +0900

    rename TARGET_FILE_FILE to TARGET_FILE_URL
    and add TARGET_FILE_NAME - is $(basename ${TARGET_FILE_URL}

--- get_latest_ssl_tools.sh
+++ get_latest_ssl_tools.sh
@@ -59,20 +59,22 @@ case "$1" in
         ;;
 esac
 
-TARGET_FILE_NAME="$(curl -Ss https://api.github.com/repos/${TARGET_TOOL_NAME}/releases | jq -r '.[0].assets[] | select(.name | test("linux_amd64")) | .browser_download_url')"
-TARGET_TOOL_VERSION="$(basename ${TARGET_FILE_NAME} | cut -d '_' -f 2)"
+TARGET_FILE_URL="$(curl -Ss https://api.github.com/repos/${TARGET_TOOL_NAME}/releases | jq -r '.[0].assets[] | select(.name | test("linux_amd64")) | .browser_download_url')"
+TARGET_TOOL_VERSION="$(basename ${TARGET_FILE_URL} | cut -d '_' -f 2)"
 cat << TARGET_INFO
 Download latest release of ${TARGET_TOOL_NAME}
 version is  : ${TARGET_TOOL_VERSION}
 platform is : linux_amd64
 TARGET_INFO
 
-FOUND_LIST="$(find ~/ -name "$(basename ${TARGET_FILE_NAME})" 2>/dev/null)"
+TARGET_FILE_NAME="$(basename "${TARGET_FILE_URL}")"
+FOUND_LIST="$(find ~/ -name "${TARGET_FILE_NAME}" 2>/dev/null)"
 if [ ! "${FOUND_LIST}" = "" ]; then
     echo "already exist on system. found at:"
     echo "${FOUND_LIST}"
     exit 0
 fi
 
-wget -q --show-progress "${TARGET_FILE_NAME}"
-chmod +x "$(basename ${TARGET_FILE_NAME})"
+trap 'rm -f "${TARGET_FILE_NAME}"; echo ;echo "Process killed. deleted downloading file $(basename ${TARGET_FILE_URL})";' 2
+wget -q --show-progress "${TARGET_FILE_URL}"
+chmod +x "${TARGET_FILE_NAME}"
